c653db040949710569c2d23312203605221c6b31
Francisco Rodrigues <francisco.rodrigues0908@gmail.com>
2026-03-03 20:17:06 +0100

    Prepare for custom deployment

---
+++ buildstatic.sh
@@ -0,0 +1,24 @@
+mkdir -p dist
+
+mv manifest.webmanifest dist/
+mv sw.js dist/
+mv sw-injector.js dist/
+
+mv *.html dist/
+mv *.png dist/
+mv *.svg dist/
+mv *.xml dist/
+
+mv audio dist/
+mv css dist/
+mv data dist/
+mv fonts dist/
+mv homebrew dist/
+mv icon dist/
+mv image dist/
+mv img dist/
+mv js
+mv lib dist/
+mv pdf dist/
+mv prerelease dist/
+mv search dist/
